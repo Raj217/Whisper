504adcd0c6ffb575c525b73978ced300076fa56f
Raj <rajdristant007@gmail.com>
2023-07-13 17:18:22 +0530

    Made it workable

--- backend/src/utils/cipher-utils.js
+++ backend/src/utils/cipher-utils.js
@@ -2,21 +2,24 @@ import { createCipheriv, randomBytes, createDecipheriv } from "crypto";
 
 class Cipher {
   static encrypt(data) {
+    const encodedFormat = "base64url";
     const { CIPHER_ALGORITHM, CIPHER_KEY } = process.env;
 
     const iv = randomBytes(16);
     const cipher = createCipheriv(CIPHER_ALGORITHM, CIPHER_KEY, iv);
+
     const encrypted =
-      cipher.update(JSON.stringify(data), "utf8", "hex") + cipher.final("hex");
-    return iv.toString("hex") + encrypted;
+      cipher.update(JSON.stringify(data), "utf8", encodedFormat) +
+      cipher.final(encodedFormat);
+    return iv.toString(encodedFormat) + encrypted;
   }
   static decrypt(encrypted) {
+    const encodedFormat = "base64url";
     const { CIPHER_ALGORITHM, CIPHER_KEY } = process.env;
-
-    const iv = Buffer.from(encrypted.slice(0, 32), "hex");
+    const iv = Buffer.from(encrypted.slice(0, 22), encodedFormat);
     const decipher = createDecipheriv(CIPHER_ALGORITHM, CIPHER_KEY, iv);
     const decrypted =
-      decipher.update(encrypted.slice(32), "hex", "utf8") +
+      decipher.update(encrypted.slice(22), encodedFormat, "utf8") +
       decipher.final("utf8");
     return JSON.parse(decrypted);
   }
