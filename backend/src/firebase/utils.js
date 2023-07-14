830f442dce54e7baf73b49a426b2589907294980
Raj <rajdristant007@gmail.com>
2023-07-14 13:18:22 +0530

    env update

--- backend/src/firebase/utils.js
+++ backend/src/firebase/utils.js
@@ -1,6 +1,5 @@
 import { OAuth2Client } from "google-auth-library";
 import admin from "firebase-admin";
-import credentials from "../../etc/secrets/firebase.json" assert { type: "json" };
 import Exception, { ExceptionCodes } from "../utils/exception.js";
 
 export const getUserDetails = async (idToken) => {
@@ -28,5 +27,18 @@ export const getUserDetails = async (idToken) => {
 };
 
 export const firebase = admin.initializeApp({
-  credential: admin.credential.cert(credentials),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_CREDENTIALS_TYPE,
+    project_id: process.env.FIREBASE_CREDENTIALS_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_CREDENTIALS_PROJECT_KEY_ID,
+    private_key: process.env.FIREBASE_CREDENTIALS_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CREDENTIALS_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CREDENTIALS_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_CREDENTIALS_AUTH_URI,
+    token_uri: process.env.FIREBASE_CREDENTIALS_TOKEN_URI,
+    auth_provider_x509_cert_url:
+      process.env.FIREBASE_CREDENTIALS_AUTH_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CREDENTIALS_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_CREDENTIALS_UNIVERSE_DOMAIN,
+  }),
 });
