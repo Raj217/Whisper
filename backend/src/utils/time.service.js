a702be6ca666dc9acda93074fca3175ac4fe7a99
Raj <rajdristant007@gmail.com>
2023-07-14 16:23:27 +0530

    Time format

--- backend/src/utils/time.service.js
+++ backend/src/utils/time.service.js
@@ -3,7 +3,7 @@ export const toIST = (date) => {
   const currentOffset = currTime.getTimezoneOffset();
   const ISTOffset = 330; // IST offset UTC +5:30
   const ISTTime = new Date(
-    date.getTime() + (ISTOffset + currentOffset) * 60000
+    date + (ISTOffset + currentOffset) * 60000
   );
   return ISTTime;
 };
