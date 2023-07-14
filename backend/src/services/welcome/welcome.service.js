bd107fff2ae84c7a54f91503e4cafba4f25a5c82
Raj <rajdristant007@gmail.com>
2023-07-14 16:00:23 +0530

    Time format

--- backend/src/services/welcome/welcome.service.js
+++ backend/src/services/welcome/welcome.service.js
@@ -1,10 +1,12 @@
+import { toIST } from "../../utils/time.service";
+
 export const welcome = async () => {
   const { VERSION } = process.env;
 
   const welcomeMessage = {
     message: `
     Welcome to whisper API v-${VERSION}
-    Current date - ${new Date()}
+    Current date - ${toIST(new Date())}
     `,
   };
   return welcomeMessage;
