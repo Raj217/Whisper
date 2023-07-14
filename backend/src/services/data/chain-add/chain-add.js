fc909cf23dad33ec7e53e319a6bd5cc49cdcc0ca
Raj <rajdristant007@gmail.com>
2023-07-14 17:48:00 +0530

    Minor tweakings

--- backend/src/services/data/chain-add/chain-add.js
+++ backend/src/services/data/chain-add/chain-add.js
@@ -25,7 +25,7 @@ export const chainAdd = async (_query, timer) => {
     if (doesContainAnyTagToScrape) {
       await scrape(chainNewTags, partialScrape);
 
-      const interval = generateRandomTime(5, 10);
+      const interval = generateRandomTime(10, 13);
 
       timer.val = setTimeout(() => {
         chainAdd({ chainNewTags }, timer);
