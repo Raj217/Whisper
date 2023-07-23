d651a803d8ffe5278b2132466a952f5000aace78
Raj <rajdristant007@gmail.com>
2023-07-23 23:33:08 +0530

    node version changes:

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -5,12 +5,6 @@ dotenv.config();
 
 const { DATABASE_URL } = process.env;
 
-/// Couldn't find MONGO_URI in env
-if (DATABASE_URL === undefined) {
-  console.log("Couldn't find mongo uri. Exiting now... ");
-  process.exit(0);
-}
-
 export const connect = () => {
   // Connect to database
   mongoose
